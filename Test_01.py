18254d5a7f84cd589b7806a71c3ff46b00233664
houshuai <houshuai>
2016-11-10 22:58:40 +0800

    2016年11月10日22:05:59 学习记录2

--- Test_01.py
+++ Test_01.py
@@ -246,12 +246,12 @@ pass 不做任何事情，一般用做占位语句。
 Python 语言 pass 语句语法格式如下：
 '''
 for letter in 'Python':
-   if letter == 'h':
-      pass
-      print ('这是 pass 块')
-   print ('当前字母 :', letter)
+    if letter == 'h':
+        pass
+        print('这是 pass 块')
+    print('当前字母 :', letter)
 
-print ("Good bye!")
+print("Good bye!")
 '''
 Python数学函数
 函数	返回值 ( 描述 )
@@ -312,4 +312,409 @@ in	成员运算符 - 如果字符串中包含给定的字符返回 True	H in a �
 not in	成员运算符 - 如果字符串中不包含给定的字符返回 True	M not in a 输出结果 1
 r/R	原始字符串 - 原始字符串：所有的字符串都是直接按照字面的意思来使用，没有转义特殊或不能打印的字符。 原始字符串除在字符串的第一个引号前加上字母"r"（可以大小写）以外，与普通字符串有着几乎完全相同的语法。	print r'\n' 输出 \n 和 print R'\n' 输出 \n
 %	格式字符串	请看下一章节
+
+
+Python字符串格式化
+Python 支持格式化字符串的输出 。尽管这样可能会用到非常复杂的表达式，但最基本的用法是将一个值插入到一个有字符串格式符 %s 的字符串中。
+在 Python 中，字符串格式化使用与 C 中 sprintf 函数一样的语法。
+如下实例：
+#!/usr/bin/python
+
+print "My name is %s and weight is %d kg!" % ('Zara', 21)
+以上实例输出结果：
+My name is Zara and weight is 21 kg!
+python字符串格式化符号:
+    符   号	描述
+      %c	 格式化字符及其ASCII码
+      %s	 格式化字符串
+      %d	 格式化整数
+      %u	 格式化无符号整型
+      %o	 格式化无符号八进制数
+      %x	 格式化无符号十六进制数
+      %X	 格式化无符号十六进制数（大写）
+      %f	 格式化浮点数字，可指定小数点后的精度
+      %e	 用科学计数法格式化浮点数
+      %E	 作用同%e，用科学计数法格式化浮点数
+      %g	 %f和%e的简写
+      %G	 %f 和 %E 的简写
+      %p	 用十六进制数格式化变量的地址
+格式化操作符辅助指令:
+符号	功能
+*	定义宽度或者小数点精度
+-	用做左对齐
++	在正数前面显示加号( + )
+<sp>	在正数前面显示空格
+#	在八进制数前面显示零('0')，在十六进制前面显示'0x'或者'0X'(取决于用的是'x'还是'X')
+0	显示的数字前面填充'0'而不是默认的空格
+%	'%%'输出一个单一的'%'
+(var)	映射变量(字典参数)
+m.n.	m 是显示的最小总宽度,n 是小数点后的位数(如果可用的话)
+Python三引号（triple quotes）
+python中三引号可以将复杂的字符串进行复制:
+python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符。
+三引号的语法是一对连续的单引号或者双引号（通常都是成对的用）。
+
+>>> hi   # repr()
+'hi\nthere'
+>>> print hi  # str()
+hi
+there
+三引号让程序员从引号和特殊字符串的泥潭里面解脱出来，自始至终保持一小块字符串的格式是所谓的WYSIWYG（所见即所得）格式的。
+一个典型的用例是，当你需要一块HTML或者SQL时，这时用字符串组合，特殊字符串转义将会非常的繁琐。
+
+Unicode 字符串
+Python 中定义一个 Unicode 字符串和定义一个普通字符串一样简单：
+>>> u'Hello World !'
+u'Hello World !'
+引号前小写的"u"表示这里创建的是一个 Unicode 字符串。如果你想加入一个特殊字符，可以使用 Python 的 Unicode-Escape 编码。如下例所示：
+>>> u'Hello\u0020World !'
+u'Hello World !'
+被替换的 \u0020 标识表示在给定位置插入编码值为 0x0020 的 Unicode 字符（空格符）。
+python的字符串内建函数
+字符串方法是从python1.6到2.0慢慢加进来的——它们也被加到了Jython中。
+这些方法实现了string模块的大部分方法，如下表所示列出了目前字符串内建支持的方法，所有的方法都包含了对Unicode的支持，有一些甚至是专门用于Unicode的。
+方法	描述
+string.capitalize()
+把字符串的第一个字符大写
+string.center(width)
+返回一个原字符串居中,并使用空格填充至长度 width 的新字符串
+string.count(str, beg=0, end=len(string))
+返回 str 在 string 里面出现的次数，如果 beg 或者 end 指定则返回指定范围内 str 出现的次数
+string.decode(encoding='UTF-8', errors='strict')
+以 encoding 指定的编码格式解码 string，如果出错默认报一个 ValueError 的 异 常 ， 除 非 errors 指 定 的 是 'ignore' 或 者'replace'
+string.encode(encoding='UTF-8', errors='strict')
+以 encoding 指定的编码格式编码 string，如果出错默认报一个ValueError 的异常，除非 errors 指定的是'ignore'或者'replace'
+string.endswith(obj, beg=0, end=len(string))
+检查字符串是否以 obj 结束，如果beg 或者 end 指定则检查指定的范围内是否以 obj 结束，如果是，返回 True,否则返回 False.
+string.expandtabs(tabsize=8)
+把字符串 string 中的 tab 符号转为空格，tab 符号默认的空格数是 8。
+string.find(str, beg=0, end=len(string))
+检测 str 是否包含在 string 中，如果 beg 和 end 指定范围，则检查是否包含在指定范围内，如果是返回开始的索引值，否则返回-1
+string.index(str, beg=0, end=len(string))
+跟find()方法一样，只不过如果str不在 string中会报一个异常.
+string.isalnum()
+如果 string 至少有一个字符并且所有字符都是字母或数字则返
+回 True,否则返回 False
+string.isalpha()
+如果 string 至少有一个字符并且所有字符都是字母则返回 True,
+否则返回 False
+string.isdecimal()
+如果 string 只包含十进制数字则返回 True 否则返回 False.
+string.isdigit()
+如果 string 只包含数字则返回 True 否则返回 False.
+string.islower()
+如果 string 中包含至少一个区分大小写的字符，并且所有这些(区分大小写的)字符都是小写，则返回 True，否则返回 False
+string.isnumeric()
+如果 string 中只包含数字字符，则返回 True，否则返回 False
+string.isspace()
+如果 string 中只包含空格，则返回 True，否则返回 False.
+string.istitle()
+如果 string 是标题化的(见 title())则返回 True，否则返回 False
+string.isupper()
+如果 string 中包含至少一个区分大小写的字符，并且所有这些(区分大小写的)字符都是大写，则返回 True，否则返回 False
+string.join(seq)
+以 string 作为分隔符，将 seq 中所有的元素(的字符串表示)合并为一个新的字符串
+string.ljust(width)
+返回一个原字符串左对齐,并使用空格填充至长度 width 的新字符串
+string.lower()
+转换 string 中所有大写字符为小写.
+string.lstrip()
+截掉 string 左边的空格
+string.maketrans(intab, outtab])
+maketrans() 方法用于创建字符映射的转换表，对于接受两个参数的最简单的调用方式，第一个参数是字符串，表示需要转换的字符，第二个参数也是字符串表示转换的目标。
+max(str)
+返回字符串 str 中最大的字母。
+min(str)
+返回字符串 str 中最小的字母。
+string.partition(str)
+有点像 find()和 split()的结合体,从 str 出现的第一个位置起,把 字 符 串 string 分 成 一 个 3 元 素 的 元 组 (string_pre_str,str,string_post_str),如果 string 中不包含str 则 string_pre_str == string.
+string.replace(str1, str2,  num=string.count(str1))
+把 string 中的 str1 替换成 str2,如果 num 指定，则替换不超过 num 次.
+string.rfind(str, beg=0,end=len(string) )
+类似于 find()函数，不过是从右边开始查找.
+string.rindex( str, beg=0,end=len(string))
+类似于 index()，不过是从右边开始.
+string.rjust(width)
+返回一个原字符串右对齐,并使用空格填充至长度 width 的新字符串
+string.rpartition(str)
+类似于 partition()函数,不过是从右边开始查找.
+string.rstrip()
+删除 string 字符串末尾的空格.
+string.split(str="", num=string.count(str))
+以 str 为分隔符切片 string，如果 num有指定值，则仅分隔 num 个子字符串
+string.splitlines(num=string.count('\n'))
+按照行分隔，返回一个包含各行作为元素的列表，如果 num 指定则仅切片 num 个行.
+string.startswith(obj, beg=0,end=len(string))
+检查字符串是否是以 obj 开头，是则返回 True，否则返回 False。如果beg 和 end 指定值，则在指定范围内检查.
+string.strip([obj])
+在 string 上执行 lstrip()和 rstrip()
+string.swapcase()
+翻转 string 中的大小写
+string.title()
+返回"标题化"的 string,就是说所有单词都是以大写开始，其余字母均为小写(见 istitle())
+string.translate(str, del="")
+根据 str 给出的表(包含 256 个字符)转换 string 的字符,
+要过滤掉的字符放到 del 参数中
+string.upper()
+转换 string 中的小写字母为大写
+string.zfill(width)
+返回长度为 width 的字符串，原字符串 string 右对齐，前面填充0
+string.isdecimal()
+isdecimal()方法检查字符串是否只包含十进制字符。这种方法只存在于unicode对象。
+'''
+'''
+-------------------------------------------
+Python 日期和时间
+Python 程序能用很多方式处理日期和时间，转换日期格式是一个常见的功能。
+Python 提供了一个 time 和 calendar 模块可以用于格式化日期和时间。
+时间间隔是以秒为单位的浮点小数。
+每个时间戳都以自从1970年1月1日午夜（历元）经过了多长时间来表示。
+Python 的 time 模块下有很多函数可以转换常见日期格式。如函数time.time()用于获取当前时间戳, 如下实例:
+
+'''
+import time;  # 引入time模块
+
+ticks = time.time()
+print("当前时间戳为:", ticks)
+localtime = time.localtime(time.time())
+print("本地时间为 :", localtime)
+localtime = time.asctime(time.localtime(time.time()))
+print("本地时间为 :", localtime)
+# 格式化成2016-03-20 11:45:39形式
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+# 格式化成Sat Mar 28 22:24:24 2016形式
+print(time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()))
+
+# 将格式字符串转换为时间戳
+a = "Sat Mar 28 22:24:24 2016"
+print(time.mktime(time.strptime(a, "%a %b %d %H:%M:%S %Y")))
+
+import calendar
+
+cal = calendar.month(2016, 1)
+print("以下输出2016年1月份的日历:")
+print(cal)
+
+'''
+定义一个函数
+你可以定义一个由自己想要功能的函数，以下是简单的规则：
+函数代码块以 def 关键词开头，后接函数标识符名称和圆括号()。
+任何传入参数和自变量必须放在圆括号中间。圆括号之间可以用于定义参数。
+函数的第一行语句可以选择性地使用文档字符串—用于存放函数说明。
+函数内容以冒号起始，并且缩进。
+return [表达式] 结束函数，选择性地返回一个值给调用方。不带表达式的return相当于返回 None。
+def functionname( parameters ):
+   "函数_文档字符串"
+   function_suite
+   return [expression]
+'''
+
+
+def printme(str):
+    "打印传入的字符串到显示器设备上"
+    print(str)
+    return
+
+
+# 调用函数
+printme("我要调用自定义函数")
+print(str)
+
+
+# 可写函数说明
+def changeme(mylist):
+    "修改传入的列表"
+    mylist.append([1, 2, 3, 4]);
+    print("函数内取值: ", mylist)
+    return
+
+
+# 调用changeme函数
+mylist = [10, 20, 30];
+changeme(mylist);
+print("函数外取值: ", mylist)
+
+'''
+你可能需要一个函数能处理比当初声明时更多的参数。这些参数叫做不定长参数，和上述2种参数不同，声明时不会命名。基本语法如下：
+def functionname([formal_args,] *var_args_tuple ):
+   "函数_文档字符串"
+   function_suite
+   return [expression]
+加了星号（*）的变量名会存放所有未命名的变量参数。选择不多传参数也可。如下实例：
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 可写函数说明
+def printinfo( arg1, *vartuple ):
+   "打印任何传入的参数"
+   print "输出: "
+   print arg1
+   for var in vartuple:
+      print var
+   return;
+
+# 调用printinfo 函数
+printinfo( 10 );
+printinfo( 70, 60, 50 );
+'''
+'''
+------------------------------
+匿名函数
+python 使用 lambda 来创建匿名函数。
+lambda只是一个表达式，函数体比def简单很多。
+lambda的主体是一个表达式，而不是一个代码块。仅仅能在lambda表达式中封装有限的逻辑进去。
+lambda函数拥有自己的命名空间，且不能访问自有参数列表之外或全局命名空间里的参数。
+虽然lambda函数看起来只能写一行，却不等同于C或C++的内联函数，后者的目的是调用小函数时不占用栈内存从而增加运行效率。
+语法
+lambda函数的语法只包含一个语句，如下：
+lambda [arg1 [,arg2,.....argn]]:expression
+如下实例：
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 可写函数说明
+sum = lambda arg1, arg2: arg1 + arg2;
+
+# 调用sum函数
+print "相加后的值为 : ", sum( 10, 20 )
+print "相加后的值为 : ", sum( 20, 20 )
+以上实例输出结果：
+相加后的值为 :  30
+相加后的值为 :  40
+
+'''
+# 可写函数
+sum = lambda arg1, arg2: arg1 + arg2;
+
+#调用sum函数
+print("相加后的值:",sum(23,5423))
+'''
+mport 语句
+想使用Python源文件，只需在另一个源文件里执行import语句，语法如下：
+import module1[, module2[,... moduleN]
+当解释器遇到import语句，如果模块在当前的搜索路径就会被导入。
+搜索路径是一个解释器会先进行搜索的所有目录的列表。如想要导入模块support.py，需要把命令放在脚本的顶端：
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 导入模块
+import support
+
+# 现在可以调用模块里包含的函数了
+support.print_func("Zara")
+以上实例输出结果：
+Hello : Zara
+一个模块只会被导入一次，不管你执行了多少次import。这样可以防止导入模块被一遍又一遍地执行。
+
+From…import 语句
+Python的from语句让你从模块中导入一个指定的部分到当前命名空间中。语法如下：
+from modname import name1[, name2[, ... nameN]]
+例如，要导入模块fib的fibonacci函数，使用如下语句：
+from fib import fibonacci
+这个声明不会把整个fib模块导入到当前的命名空间中，它只会将fib里的fibonacci单个引入到执行这个声明的模块的全局符号表。
+
+From…import* 语句
+把一个模块的所有内容全都导入到当前的命名空间也是可行的，只需使用如下声明：
+from modname import *
+这提供了一个简单的方法来导入一个模块中的所有项目。然而这种声明不该被过多地使用。
+
+定位模块
+当你导入一个模块，Python解析器对模块位置的搜索顺序是：
+当前目录
+如果不在当前目录，Python 则搜索在 shell 变量 PYTHONPATH 下的每个目录。
+如果都找不到，Python会察看默认路径。UNIX下，默认路径一般为/usr/local/lib/python/。
+模块搜索路径存储在system模块的sys.path变量中。变量里包含当前目录，PYTHONPATH和由安装过程决定的默认目录。
+
+PYTHONPATH变量
+作为环境变量，PYTHONPATH由装在一个列表里的许多目录组成。PYTHONPATH的语法和shell变量PATH的一样。
+在Windows系统，典型的PYTHONPATH如下：
+set PYTHONPATH=c:\python20\lib;
+在UNIX系统，典型的PYTHONPATH如下：
+set PYTHONPATH=/usr/local/lib/python
+命名空间和作用域
+变量是拥有匹配对象的名字（标识符）。命名空间是一个包含了变量名称们（键）和它们各自相应的对象们（值）的字典。
+一个Python表达式可以访问局部命名空间和全局命名空间里的变量。如果一个局部变量和一个全局变量重名，则局部变量会覆盖全局变量。
+每个函数都有自己的命名空间。类的方法的作用域规则和通常函数的一样。
+Python会智能地猜测一个变量是局部的还是全局的，它假设任何在函数内赋值的变量都是局部的。
+因此，如果要给全局变量在一个函数里赋值，必须使用global语句。
+global VarName的表达式会告诉Python， VarName是一个全局变量，这样Python就不会在局部命名空间里寻找这个变量了。
+例如，我们在全局命名空间里定义一个变量money。我们再在函数内给变量money赋值，然后Python会假定money是一个局部变量。然而，我们并没有在访问前声明一个局部变量money，结果就是会出现一个UnboundLocalError的错误。取消global语句的注释就能解决这个问题。
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+Money = 2000
+def AddMoney():
+   # 想改正代码就取消以下注释:
+   # global Money
+   Money = Money + 1
+
+print Money
+AddMoney()
+print Money
+dir()函数
+dir()函数一个排好序的字符串列表，内容是一个模块里定义过的名字。
+返回的列表容纳了在一个模块里定义的所有模块，变量和函数。如下一个简单的实例：
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 导入内置math模块
+import math
+
+content = dir(math)
+
+print content;
+以上实例输出结果：
+['__doc__', '__file__', '__name__', 'acos', 'asin', 'atan',
+'atan2', 'ceil', 'cos', 'cosh', 'degrees', 'e', 'exp',
+'fabs', 'floor', 'fmod', 'frexp', 'hypot', 'ldexp', 'log',
+'log10', 'modf', 'pi', 'pow', 'radians', 'sin', 'sinh',
+'sqrt', 'tan', 'tanh']
+在这里，特殊字符串变量__name__指向模块的名字，__file__指向该模块的导入文件名。
+
+globals()和locals()函数
+根据调用地方的不同，globals()和locals()函数可被用来返回全局和局部命名空间里的名字。
+如果在函数内部调用locals()，返回的是所有能在该函数里访问的命名。
+如果在函数内部调用globals()，返回的是所有在该函数里能访问的全局名字。
+两个函数的返回类型都是字典。所以名字们能用keys()函数摘取。
+
+reload()函数
+当一个模块被导入到一个脚本，模块顶层部分的代码只会被执行一次。
+因此，如果你想重新执行模块里顶层部分的代码，可以用reload()函数。该函数会重新导入之前导入过的模块。语法如下：
+reload(module_name)
+在这里，module_name要直接放模块的名字，而不是一个字符串形式。比如想重载hello模块，如下：
+reload(hello)
+Python中的包
+包是一个分层次的文件目录结构，它定义了一个由模块及子包，和子包下的子包等组成的Python的应用环境。
+考虑一个在Phone目录下的pots.py文件。这个文件有如下源代码：
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def Pots():
+   print "I'm Pots Phone"
+
+同样地，我们有另外两个保存了不同函数的文件：
+Phone/Isdn.py 含有函数Isdn()
+Phone/G3.py 含有函数G3()
+现在，在Phone目录下创建file __init__.py：
+Phone/__init__.py
+当你导入Phone时，为了能够使用所有函数，你需要在__init__.py里使用显式的导入语句，如下：
+from Pots import Pots
+from Isdn import Isdn
+from G3 import G3
+当你把这些代码添加到__init__.py之后，导入Phone包的时候这些类就全都是可用的了。
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 导入 Phone 包
+import Phone
+
+Phone.Pots()
+Phone.Isdn()
+Phone.G3()
+以上实例输出结果：
+I'm Pots Phone
+I'm 3G Phone
+I'm ISDN Phone
+如上，为了举例，我们只在每个文件里放置了一个函数，但其实你可以放置许多函数。你也可以在这些文件里定义Python的类，然后为这些类建一个包。
 '''
