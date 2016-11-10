92b5f8b3de729c8d58d85b70cfbe43f154aed820
houshuai <houshuai>
2016-11-10 22:06:14 +0800

    2016年11月10日22:05:59 学习记录

--- Test_01.py
+++ Test_01.py
@@ -60,3 +60,256 @@ a, b, c = 1, 2, "john"
 '''
 a, b, c = 1, 2, "john"
 a = b = c = 1
+'''
+Python字符串
+字符串或串(String)是由数字、字母、下划线组成的一串字符。
+一般记为 :
+s="a1a2···an"(n>=0)
+它是编程语言中表示文本的数据类型。
+python的字串列表有2种取值顺序:
+从左到右索引默认0开始的，最大范围是字符串长度少1
+从右到左索引默认-1开始的，最大范围是字符串开头
+如果你的实要取得一段子串的话，可以用到变量[头下标:尾下标]，就可以截取相应的字符串，其中下标是从0开始算起，可以是正数或负数，下标可以为空表示取到头或尾。
+比如:
+s = 'ilovepython'
+s[1:5]的结果是love。
+当使用以冒号分隔的字符串，python返回一个新的对象，结果包含了以这对偏移标识的连续的内容，左边的开始是包含了下边界。
+上面的结果包含了s[1]的值l，而取到的最大范围不包括上边界，就是s[5]的值p。
+'''
+s = "iLovePython"
+print(s[1:5])  # 输出字符串第二个字符开始到第四个字符结束
+
+list = ['runoob', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+'''
+Python列表
+List（列表） 是 Python 中使用最频繁的数据类型。
+列表可以完成大多数集合类的数据结构实现。它支持字符，数字，字符串甚至可以包含列表（所谓嵌套）。
+列表用[ ]标识。是python最通用的复合数据类型。看这段代码就明白。
+列表中的值得分割也可以用到变量[头下标:尾下标]，就可以截取相应的列表，从左到右索引默认0开始的，从右到左索引默认-1开始，下标可以为空表示取到头或尾。
+加号（+）是列表连接运算符，星号（*）是重复操作。如下实例：
+'''
+list = ['runoob', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+print(list)  # 输出完整列表
+print(list[0])  # 输出列表的第一个元素
+print(list[1:3])  # 输出第二个至第三个的元素
+print(list[2:])  # 输出从第三个开始至列表末尾的所有元素
+print(tinylist * 2)  # 输出列表两次
+print(list + tinylist)  # 打印组合的列表
+
+'''
+Python元组
+元组是另一个数据类型，类似于List（列表）。
+元组用"()"标识。内部元素用逗号隔开。但是元组不能二次赋值，相当于只读列表。
+'''
+tuple = ('runoob', 786, 2.23, 'john', 70.2)
+tinytuple = (123, 'john')
+
+print(tuple)  # 输出完整元组
+print(tuple[2:])  # 输出从第三个开始至列表末尾的所有元素
+print(tinytuple * 2)  # 输出元组两次
+print(tuple + tinytuple)  # 打印组合的元组
+
+'''
+Python元字典
+字典(dictionary)是除列表以外python之中最灵活的内置数据结构类型。列表是有序的对象结合，字典是无序的对象集合。
+两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+字典用"{ }"标识。字典由索引(key)和它对应的值value组成。
+'''
+
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+
+print(dict['one'])  # 输出键为'one' 的值
+print(dict[2])  # 输出键为 2 的值
+print(tinydict)  # 输出完整的字典
+print(tinydict.keys())  # 输出所有键
+print(tinydict.values())  # 输出所有值
+
+'''
+Python数据类型转换
+有时候，我们需要对数据内置的类型进行转换，数据类型的转换，你只需要将数据类型作为函数名即可。
+以下几个内置的函数可以执行数据类型之间的转换。这些函数返回一个新的对象，表示转换的值。
+函数	描述
+int(x [,base])
+将x转换为一个整数
+long(x [,base] )
+将x转换为一个长整数
+float(x)
+将x转换到一个浮点数
+complex(real [,imag])
+创建一个复数
+str(x)
+将对象 x 转换为字符串
+repr(x)
+将对象 x 转换为表达式字符串
+eval(str)
+用来计算在字符串中的有效Python表达式,并返回一个对象
+tuple(s)
+将序列 s 转换为一个元组
+list(s)
+将序列 s 转换为一个列表
+set(s)
+转换为可变集合
+dict(d)
+创建一个字典。d 必须是一个序列 (key,value)元组。
+frozenset(s)
+转换为不可变集合
+chr(x)
+将一个整数转换为一个字符
+unichr(x)
+将一个整数转换为Unicode字符
+ord(x)
+将一个字符转换为它的整数值
+hex(x)
+将一个整数转换为一个十六进制字符串
+oct(x)
+将一个整数转换为一个八进制字符串
+
+'''
+'''
+---------------------------------------------------
+'''
+'''
+Python成员运算符
+除了以上的一些运算符之外，Python还支持成员运算符，测试实例中包含了一系列的成员，包括字符串，列表或元组。
+运算符	描述	实例
+in	如果在指定的序列中找到值返回 True，否则返回 False。	x 在 y 序列中 , 如果 x 在 y 序列中返回 True。
+not in	如果在指定的序列中没有找到值返回 True，否则返回 False。	x 不在 y 序列中 , 如果 x 不在 y 序列中返回 True
+
+'''
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5];
+
+if (a in list):
+    print("1 - 变量 a 在给定的列表中 list 中")
+else:
+    print("1 - 变量 a 不在给定的列表中 list 中")
+
+if (b not in list):
+    print("2 - 变量 b 不在给定的列表中 list 中")
+else:
+    print("2 - 变量 b 在给定的列表中 list 中")
+
+# 修改变量 a 的值
+a = 2
+if (a in list):
+    print("3 - 变量 a 在给定的列表中 list 中")
+else:
+    print("3 - 变量 a 不在给定的列表中 list 中")
+'''
+    Python身份运算符
+ 身份运算符用于比较两个对象的存储单元
+ 运算符	描述	实例
+ is	is是判断两个标识符是不是引用自一个对象	x is y, 如果 id(x) 等于 id(y) , is 返回结果 1
+ is not	is not是判断两个标识符是不是引用自不同对象	x is not y, 如果 id(x) 不等于 id(y). is not 返回结果 1
+'''
+a = 20
+b = 20
+
+if (a is b):
+    print("1 - a 和 b 有相同的标识")
+else:
+    print("1 - a 和 b 没有相同的标识")
+
+    if (id(a) == id(b)):
+        print(
+            "2 - a 和 b 有相同的标识")
+    else:
+        print
+        ("2 - a 和 b 没有相同的标识")
+
+    # 修改变量 b 的值
+    b = 30
+    if (a is b):
+        print
+        ("3 - a 和 b 有相同的标识")
+    else:
+        print
+        ("3 - a 和 b 没有相同的标识")
+
+    if (a is not b):
+        print
+        ("4 - a 和 b 没有相同的标识")
+    else:
+        print
+        ("4 - a 和 b 有相同的标识")
+'''
+Python pass 语句
+Python pass是空语句，是为了保持程序结构的完整性。
+pass 不做任何事情，一般用做占位语句。
+Python 语言 pass 语句语法格式如下：
+'''
+for letter in 'Python':
+   if letter == 'h':
+      pass
+      print ('这是 pass 块')
+   print ('当前字母 :', letter)
+
+print ("Good bye!")
+'''
+Python数学函数
+函数	返回值 ( 描述 )
+abs(x)	返回数字的绝对值，如abs(-10) 返回 10
+ceil(x)	返回数字的上入整数，如math.ceil(4.1) 返回 5
+cmp(x, y)	如果 x < y 返回 -1, 如果 x == y 返回 0, 如果 x > y 返回 1
+exp(x)	返回e的x次幂(ex),如math.exp(1) 返回2.718281828459045
+fabs(x)	返回数字的绝对值，如math.fabs(-10) 返回10.0
+floor(x)	返回数字的下舍整数，如math.floor(4.9)返回 4
+log(x)	如math.log(math.e)返回1.0,math.log(100,10)返回2.0
+log10(x)	返回以10为基数的x的对数，如math.log10(100)返回 2.0
+max(x1, x2,...)	返回给定参数的最大值，参数可以为序列。
+min(x1, x2,...)	返回给定参数的最小值，参数可以为序列。
+modf(x)	返回x的整数部分与小数部分，两部分的数值符号与x相同，整数部分以浮点型表示。
+pow(x, y)	x**y 运算后的值。
+round(x [,n])	返回浮点数x的四舍五入值，如给出n值，则代表舍入到小数点后的位数。
+sqrt(x)	返回数字x的平方根，数字可以为负数，返回类型为实数，如math.sqrt(4)返回 2+0j
+
+Python随机数函数
+随机数可以用于数学，游戏，安全等领域中，还经常被嵌入到算法中，用以提高算法效率，并提高程序的安全性。
+Python包含以下常用随机数函数：
+函数	描述
+choice(seq)	从序列的元素中随机挑选一个元素，比如random.choice(range(10))，从0到9中随机挑选一个整数。
+randrange ([start,] stop [,step])	从指定范围内，按指定基数递增的集合中获取一个随机数，基数缺省值为1
+random()	随机生成下一个实数，它在[0,1)范围内。
+seed([x])	改变随机数生成器的种子seed。如果你不了解其原理，你不必特别去设定seed，Python会帮你选择seed。
+shuffle(lst)	将序列的所有元素随机排序
+uniform(x, y)	随机生成下一个实数，它在[x,y]范围内。
+
+Python三角函数
+Python包括以下三角函数：
+函数	描述
+acos(x)	返回x的反余弦弧度值。
+asin(x)	返回x的反正弦弧度值。
+atan(x)	返回x的反正切弧度值。
+atan2(y, x)	返回给定的 X 及 Y 坐标值的反正切值。
+cos(x)	返回x的弧度的余弦值。
+hypot(x, y)	返回欧几里德范数 sqrt(x*x + y*y)。
+sin(x)	返回的x弧度的正弦值。
+tan(x)	返回x弧度的正切值。
+degrees(x)	将弧度转换为角度,如degrees(math.pi/2) ， 返回90.0
+radians(x)	将角度转换为弧度
+
+Python数学常量
+常量	描述
+pi	数学常量 pi（圆周率，一般以π来表示）
+e	数学常量 e，e即自然常数（自然常数）。
+
+
+Python字符串运算符
+下表实例变量a值为字符串"Hello"，b变量值为"Python"：
+操作符	描述	实例
++	字符串连接	a + b 输出结果： HelloPython
+*	重复输出字符串	a*2 输出结果：HelloHello
+[]	通过索引获取字符串中字符	a[1] 输出结果 e
+[ : ]	截取字符串中的一部分	a[1:4] 输出结果 ell
+in	成员运算符 - 如果字符串中包含给定的字符返回 True	H in a 输出结果 1
+not in	成员运算符 - 如果字符串中不包含给定的字符返回 True	M not in a 输出结果 1
+r/R	原始字符串 - 原始字符串：所有的字符串都是直接按照字面的意思来使用，没有转义特殊或不能打印的字符。 原始字符串除在字符串的第一个引号前加上字母"r"（可以大小写）以外，与普通字符串有着几乎完全相同的语法。	print r'\n' 输出 \n 和 print R'\n' 输出 \n
+%	格式字符串	请看下一章节
+'''
